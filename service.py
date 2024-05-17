de6af621680e04c66871ffbb3811dc981332b047
Sam Champagne <sam.c.tur@gmail.com>
2024-05-16 20:47:28 -0600

    added basic auth

--- service.py
+++ service.py
@@ -1,13 +1,20 @@
+from flask_basicauth import BasicAuth
 from flask import Flask, jsonify
 import json
 
 app = Flask(__name__)
 
-# Load malicious URLs from JSON file / DB
+# Load malicious URLs from JSON file
 with open('malicious_urls.json', 'r') as f:
     MALICIOUS_URLS = json.load(f)['urls']
 
+# Configure basic authentication
+app.config['BASIC_AUTH_USERNAME'] = 'username'
+app.config['BASIC_AUTH_PASSWORD'] = 'password'
+basic_auth = BasicAuth(app)
+
 @app.route('/v1/urlinfo/<path:resource_url>', methods=['GET'])
+@basic_auth.required
 def check_url(resource_url):
     if resource_url in MALICIOUS_URLS:
         return jsonify({'url': resource_url, 'safe': False, 'message': 'This URL is known to contain malware'}), 200
@@ -15,5 +22,5 @@ def check_url(resource_url):
         return jsonify({'url': resource_url, 'safe': True, 'message': 'This URL is safe to access'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
